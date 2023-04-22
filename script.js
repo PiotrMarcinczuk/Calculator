65bd843fd58784076e1d80c5b7da70270c3ed8c9
dzw <100282489+PiotrMarcinczuk@users.noreply.github.com>
2023-04-22 20:49:03 +0200

    Implementacja systemu +/- naprawa kilku błędów

--- script.js
+++ script.js
@@ -9,15 +9,17 @@ const equal = document.querySelectorAll('.equal');
 
 let current_number = 0;
 let current_operator = '';
-
+let tab_of_operations = ['%','C','UNDO','1/x','+/-','/','*','+','-'];
 sign.forEach((button) => button.addEventListener('click', operate));
 number.forEach((button) => button.addEventListener('click', display_number));
 equal.forEach((button) => button.addEventListener('click', check_operator));
 function check_operator(){
-    let operator_for_equal = '';
-    operator_for_equal = current_operator;
-    console.log(operator_for_equal);
-    math_for_equal(operator_for_equal);
+    if(!p_previous.textContent.includes('=')){
+        let operator_for_equal = '';
+        operator_for_equal = current_operator;
+        console.log(operator_for_equal);
+        math_for_equal(operator_for_equal);
+    }  
 }
 clear.forEach((button) => {
     button.addEventListener('click', () => {
@@ -27,8 +29,12 @@ clear.forEach((button) => {
 })
 
 function display_number(){
-    if(this.textContent === '.' && p_current.textContent === ''){
-        p_current.textContent = '0.'
+    if(this.textContent === '.' && (p_current.textContent === '' || p_current.textContent === '-')){
+        if(p_current.textContent === '-'){
+            p_current.textContent = '-0.'
+        }else{
+            p_current.textContent = '0.'
+        }
     }else if(p_current.textContent !== '' && !p_current.textContent.includes('.')){
         p_current.textContent += this.textContent;
     }else if(this.textContent !== '.' && p_current.textContent.includes('.')){
@@ -38,23 +44,36 @@ function display_number(){
             p_current.textContent += this.textContent;
         }
     }
+    if(p_previous.textContent.includes('=')){
+        p_current.textContent = '';
+        p_previous.textContent = '';
+        p_current.textContent += this.textContent;
+    }
 }
-// kiedy mnozmym np -69 * to tu nie mozemy wpisac -liczby
 function operate(){
     if(p_current.textContent !== '' && p_current.textContent !== '-'){
         if(p_previous.textContent === ''){
-            current_operator = this.textContent;
-            current_number = p_current.textContent;
-            p_previous.textContent = current_number;   
-            p_previous.textContent += current_operator;
+            if(this.textContent === '+/-'){
+                current_operator = this.textContent;
+                current_number = p_current.textContent;
+                p_previous.textContent = current_number;
+                math(current_operator);
+            }else{
+                current_operator = this.textContent;
+                current_number = p_current.textContent;
+                p_previous.textContent = current_number;   
+                p_previous.textContent += current_operator;
+            }   
         }else{ 
             if(this.textContent && !p_previous.textContent.includes(this.textContent)){ // tu moga byc problemy z bardziej zlozonymi dzialaniami
                 console.log(this.textContent);
                 console.log(current_operator)
                 console.log(432534534);
-                math(current_operator);
+                console.log('piewrszy math'); //cos tu
+                math(current_operator); // wczesniej poprostu math bez ifas
                 p_previous.textContent += this.textContent;
-            }else{
+            }else if(!p_previous.textContent.includes('=')){// dodane warunek if wczesniej samo else
+                console.log('drugi math');
                 math(current_operator);
             p_previous.textContent += current_operator;
             current_number = parseFloat(p_previous.textContent);   
@@ -67,18 +86,25 @@ function operate(){
             console.log('cos z -');
             console.log(this.textContent);
             console.log(current_operator);
-        } 
+        }else if(this.textContent === '-' && !p_current.textContent.includes('-')){
+            p_current.textContent += this.textContent;
+        }
     }
     if(!p_current.textContent.includes('-')){
         current_operator = this.textContent;
     }
+    if(p_previous.textContent !== '' && p_current.textContent === '' && !tab_of_operations.includes(p_previous.textContent.slice(-1)) ){
+        if(this.textContent !== '+/-'){
+            p_previous.textContent += this.textContent;
+            current_operator = this.textContent;
+        } 
+    }
 }
 
 function math(current_operator){
-
     switch(current_operator){
         case '+':
-            if(p_previous.textContent.includes('=')){
+            if(p_previous.textContent.includes('=')){ // ty '=' byla nie wiem dlaczego
                 p_previous.textContent = parseFloat(p_current.textContent);
             }else{
                 p_previous.textContent = parseFloat(p_previous.textContent) + parseFloat(p_current.textContent);
@@ -92,6 +118,9 @@ function math(current_operator){
             }
             break; 
         case '-':
+            // kiedy pierwsza liczba zaczyna sie od -8 to zle mnozy
+            // bugi z '+/-'
+            // czasem niepoprawne operacje
             console.log(parseFloat(p_previous.textContent));
             p_previous.textContent = parseFloat(p_previous.textContent) - parseFloat(p_current.textContent);
             if(p_current.textContent.includes('.')){
@@ -124,6 +153,18 @@ function math(current_operator){
                 p_current.textContent = parseFloat(p_current.textContent).toFixed(3) * -1;
             }
             break;
+        case '+/-':
+            console.log(parseFloat(p_previous.textContent));
+            p_previous.textContent = parseFloat(p_previous.textContent) * -1;
+            if(p_current.textContent.includes('.')){
+                p_previous.textContent = parseFloat(p_previous.textContent).toFixed(3)
+            }   
+            if(p_current.textContent.includes('-')){
+                console.log('zamiana * -1');
+                p_current.textContent = parseFloat(p_current.textContent).toFixed(3) * -1;
+            }
+            break;
+
     }
 }
 
